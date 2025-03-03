1245688dc745a7bdf39e2b162fdc80a0b5127936
FlipDark <sanderbenning2007@gmail.com>
2025-03-03 15:54:06 +0100

    Splitting rooms vertically

--- AlgorithmsProject/Assets/Scripts/DungeonGenerator.cs
+++ AlgorithmsProject/Assets/Scripts/DungeonGenerator.cs
@@ -5,12 +5,12 @@ public class DungeonGenerator : MonoBehaviour
 {
 
     [Header("Dungeon Size")]
-    [SerializeField] private int DungeonWidth = 100;
-    [SerializeField] private int DungeonLength = 50;
+    [SerializeField] private int DungeonWidth;
+    [SerializeField] private int DungeonLength;
 
     [Header("Dungeon Rooms")]
-    [SerializeField] private int RoomLength = 100;
-    [SerializeField] private int RoomWidth = 50;
+    [SerializeField] private int RoomLength;
+    [SerializeField] private int RoomWidth;
 
     List<RectInt> rooms;
 
@@ -20,43 +20,33 @@ public class DungeonGenerator : MonoBehaviour
         rooms = new List<RectInt>();
         rooms.Add(new RectInt(0, 0, DungeonWidth, DungeonLength));
 
+        CreateRooms();
+
+    }
+    private void Update()
+    {
+        foreach (RectInt room in rooms) AlgorithmsUtils.DebugRectInt(room, Color.yellow);
     }
 
     private void CreateRooms()
     {
-        for(int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
-            foreach (RectInt room in rooms)
+            for (int k = rooms.Count - 1; k >= 0; k--)
             {
-                // randomly splitting yes or no?
-                if(Random.Range(1,2) == 1)
-                {
-                    if(Random.Range(1,2) == 1)
-                    {
-                        SplitRoomVertically(room);
-                        
-                    }
-                }
-
-                // if yes randomly splitting over x or y?
-
-                // remove original room if it was split
+                SplitRoomVertically(rooms[k]);
             }
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        foreach (RectInt room in rooms)
-        {
-            AlgorithmsUtils.DebugRectInt(room, Color.red);
         }
     }
 
     private void SplitRoomVertically(RectInt room)
     {
+        int splitX = room.x + room.width / 2;
+
         RectInt roomLeft = new RectInt(0, 0, RoomWidth / 2, RoomLength);
         RectInt roomRight = new RectInt(0 + RoomWidth / 2, 0, RoomWidth / 2, RoomLength);
-        
+
+        rooms.Add(roomLeft);
+        rooms.Add(roomRight);
     }
 }
